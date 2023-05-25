69fd7f11707949a6c3292bf4d9fc0a5a3ccf14bb
Izth4rDev <miguel.rondanelli.inf@gmail.com>
2023-05-25 16:24:07 -0400

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <stored-resource :resources="storedResources"></stored-resource>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StoredResource from './components/learning-resources/StoredResource.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default{
+    components:{
+        StoredResource
+    },
+    data(){
+        return{
+            storedResources:[
+                {
+                    id:'official-guide',
+                    tittle:'official Guide',
+                    decription:'The official Vue.js documentation', 
+                    link:'https://vuejs.org'
+                },
+                {
+                    id:'Google',
+                    tittle:'Google',
+                    decription:'Learn to google', 
+                    link:'https://google.com'
+                },
+            ]
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/learning-resources/StoredResource.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul>
+        <list-resource v-for="resource in resources" :key="resource.id"
+            :tittle="resource.tittle"
+            :description="resource.decription"
+            :link="resource.link"
+        ></list-resource>
+    </ul>
+</template>
+
+<script>
+
+import ListResource from './ListResource.vue';
+
+export default{
+    components:{
+        ListResource
+    },
+    props:['resources']
+}
+</script>
